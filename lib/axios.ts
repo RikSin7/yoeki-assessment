e41cc2dc9b7ed973274c81c30d63135299d08917
Ritik <ritik@logiqlink.com>
2026-05-28 21:20:44 +0530

    feat: implement layout Redux provider, axios API client, and custom Redux hooks

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || '',
+export const api = axios.create({
+  baseURL: "/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-
-export default axiosInstance;
